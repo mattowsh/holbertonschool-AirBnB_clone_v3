de069c974c7fa0f429c6a773e082e51d3e8fc861
mattowsh <mattobelen@gmail.com>
2022-12-11 22:18:00 +0000

    task 11 new try, user import

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Place endpoints """
-from flask import jsonify, request, abort
+from api.v1.views import app_views
+from flask import jsonify, abort, request
 from models import storage
 from models.place import Place
 from models.user import User
-from api.v1.views import app_views
 
 
 @app_views.route('/cities/<string:city_id>/places', methods=["GET", "POST"])
